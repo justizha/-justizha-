7ccf2b263f5a8d5494a372edd2a4c5e302184b30
justizha <alfathizhaaaaaaa@gmail.com>
2024-06-18 11:11:59 +0700

    update

--- src/components/Languages.tsx
+++ src/components/Languages.tsx
@@ -1,14 +1,14 @@
 export default function Languages() {
   return (
     <section className="mx-auto">
-      <ol className="grid w-full list-none grid-cols-2 gap-4 text-lg text-gray-500 dark:text-gray-400 sm:grid-cols-4">
+      <ol className="grid w-full list-none grid-cols-2 gap-4 place-self-center text-lg text-gray-500 dark:text-gray-400 sm:grid-cols-4">
         {/* js */}
         <li className="rounded-xl border bg-gradient-to-br from-white to-gray-50 p-5 text-xl transition duration-200 hover:-translate-y-1 hover:shadow-lg  dark:border-gray-700  dark:from-gray-700/75 dark:to-gray-800/75">
           <div className="flex h-full w-full flex-col items-center justify-center gap-3">
             <img
               src="/img/javascript.png"
               alt="javascript"
-              className="absolute w-2 opacity-10 blur-md filter md:w-56"
+              className="absolute w-2 opacity-10 blur-md filter md:w-48"
             />
             <img
               src="/img/javascript.png"
@@ -23,8 +23,8 @@ export default function Languages() {
           <div className="flex h-full w-full flex-col items-center justify-center gap-3">
             <img
               src="/img/typescript.png"
-              alt="php"
-              className="absolute w-2 opacity-10 blur-md filter md:w-56"
+              alt="typescript"
+              className="absolute w-2 opacity-10 blur-md filter md:w-48"
             />
             <img
               src="/img/typescript.png"
@@ -40,7 +40,7 @@ export default function Languages() {
             <img
               src="/img/php.png"
               alt="php"
-              className="absolute w-2 opacity-10 blur-md filter md:w-56"
+              className="absolute w-2 opacity-10 blur-md filter md:w-48"
             />
             <img
               src="/img/php.png"
@@ -57,7 +57,7 @@ export default function Languages() {
             <img
               src="/img/tailwind.png"
               alt="tailwind"
-              className="absolute w-2 opacity-10 blur-md filter md:w-56"
+              className="absolute w-2 opacity-10 blur-md filter md:w-48"
             />
             <img
               src="/img/tailwind.png"
@@ -68,15 +68,15 @@ export default function Languages() {
           </div>
         </li>
         {/* react */}
-        <li className="rounded-xl border bg-gradient-to-br from-white to-gray-50 p-5 text-xl transition duration-200 hover:-translate-y-1 hover:shadow-lg  dark:border-gray-700  dark:from-gray-700/75 dark:to-gray-800/75">
+        <li className="rounded-xl border bg-gradient-to-br from-white via-gray-50 to-gray-100 p-5 text-xl transition duration-200 hover:-translate-y-1  dark:border-gray-700  dark:from-gray-700/75 dark:to-gray-800/75">
           <div className="flex h-full w-full flex-col items-center justify-center gap-3">
             <img
               src="/img/reactjs.png"
-              alt="tailwind"
-              className="absolute w-2 opacity-10 blur-md filter md:w-56"
+              alt="react"
+              className="absolute w-2 opacity-10 blur-md filter md:w-48"
             />
             <img
-              src="/img/reactjs.png"
+              src="/img/react.png"
               alt="react"
               className="w-20 md:w-36"
               loading="lazy"
@@ -89,11 +89,43 @@ export default function Languages() {
             <img
               src="/img/next.png"
               alt="tailwind"
-              className="absolute w-2 opacity-10 blur-md filter md:w-56"
+              className="absolute w-2 opacity-10 blur-md filter md:w-48"
             />
             <img
               src="/img/next.png"
               alt="react"
+              className="w-20 md:w-36"
+              loading="lazy"
+            />
+          </div>
+        </li>
+        {/* laravel */}
+        <li className="hover:shadow-l g transit200 rounded-xl border bg-gradient-to-br from-white to-gray-50 p-5 text-xl duration-200 hover:-translate-y-1 dark:border-gray-700  dark:from-gray-700/75 dark:to-gray-800/75">
+          <div className="flex h-full w-full flex-col items-center justify-center gap-3">
+            <img
+              src="/img/laravel.png"
+              alt="laravel"
+              className="absolute w-2 opacity-10 blur-md filter md:w-48"
+            />
+            <img
+              src="/img/laravel.png"
+              alt="laravel"
+              className="w-20 md:w-36"
+              loading="lazy"
+            />
+          </div>
+        </li>
+        {/* vue */}
+        <li className="hover:shadow-l g transit200 rounded-xl border bg-gradient-to-br from-white to-gray-50 p-5 text-xl duration-200 hover:-translate-y-1 dark:border-gray-700  dark:from-gray-700/75 dark:to-gray-800/75">
+          <div className="flex h-full w-full flex-col items-center justify-center gap-3">
+            <img
+              src="/img/vue.png"
+              alt="vue"
+              className="absolute w-2 opacity-10 blur-md filter md:w-48"
+            />
+            <img
+              src="/img/vue.png"
+              alt="vue"
               className="w-20 md:w-36"
               loading="lazy"
             />
