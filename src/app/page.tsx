03319788466adc2ee1c3e81dab438de758e3c221
justizha <alfathizhaaaaaaa@gmail.com>
2024-06-18 10:55:52 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,31 @@
+import Languages from "../components/Languages";
+
 export default function Home() {
   return (
-    <section className="py-36">
-      <div className=" text-center">
-        <h1 className="text-5xl font-black tracking-wider md:text-6xl lg:text-9xl">
-          Just
-          <span className="bg-indigo-500 p-[2px] text-white dark:bg-indigo-800">
-            Izha.
-          </span>
-        </h1>
-      </div>
-      <div className="pt-16">
-        <h2 className="mb-2 text-3xl font-medium">Hello There !</h2>
-        <div className="text-lg tracking-wide">
-          <p className="mb-2">
-            I'm Izha! I'm currently working as a Frontend Web Developer.
-          </p>
-          <p>
-            I am passionate about web design, artificial intelligence, UI/UX
-            ,e-sports , and psychology.
-          </p>
+    <>
+      <section className="py-36">
+        <div className=" text-center">
+          <h1 className="text-5xl font-black tracking-wider md:text-6xl lg:text-9xl">
+            Just
+            <span className="bg-indigo-500 p-[2px] text-white dark:bg-indigo-800">
+              Izha.
+            </span>
+          </h1>
         </div>
-      </div>
-    </section>
+        <div className="pt-16">
+          <h2 className="mb-2 text-3xl font-medium">Hello There !</h2>
+          <div className="text-lg tracking-wide">
+            <p className="mb-2">
+              I'm Izha! I'm currently working as a Frontend Web Developer.
+            </p>
+            <p>
+              I am passionate about web design, artificial intelligence, UI/UX
+              ,e-sports , and psychology.
+            </p>
+          </div>
+        </div>
+      </section>
+      <Languages />
+    </>
   );
 }
--- src/components/Languages.tsx
+++ src/components/Languages.tsx
@@ -1,113 +1,105 @@
 export default function Languages() {
   return (
     <section className="mx-auto">
-      <div className="flex-none gap-8 md:flex">
-        <div>
-          <h1>Some Title</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius,
-            repellendus.
-          </p>
-        </div>
-        <ol className="grid aspect-square max-w-md grid-cols-2 gap-3">
-          {/* html */}
-          <li className="rounded-xl border bg-gradient-to-br from-white to-gray-50 p-5 text-xl duration-200 hover:shadow-lg dark:border-gray-700  dark:from-gray-700/75 dark:to-gray-800/75">
-            <div className="flex h-full w-full flex-col items-center justify-center gap-3">
-              <img
-                src="/img/html.png"
-                alt="html"
-                className="absolute w-24 opacity-20 blur-md filter md:w-56"
-              />
-              <img
-                src="/img/html.png"
-                alt="html"
-                className="w-20 md:w-36"
-                loading="lazy"
-              />
-            </div>
-          </li>
-          {/* css */}
-          <li className="rounded-xl border bg-gradient-to-br from-white to-gray-50 p-5 text-xl duration-200 hover:shadow-lg dark:border-gray-700  dark:from-gray-700/75 dark:to-gray-800/75">
-            <div className="flex h-full w-full flex-col items-center justify-center gap-3">
-              <img
-                src="/img/css.png"
-                alt="html"
-                className="absolute w-24 opacity-20 blur-md filter md:w-56"
-              />
-              <img
-                src="/img/css.png"
-                alt="html"
-                className="w-20 md:w-36"
-                loading="lazy"
-              />
-            </div>
-          </li>
-          {/* js */}
-          <li className="rounded-xl border bg-gradient-to-br from-white to-gray-50 p-5 text-xl duration-200 hover:shadow-lg dark:border-gray-700  dark:from-gray-700/75 dark:to-gray-800/75">
-            <div className="flex h-full w-full flex-col items-center justify-center gap-3">
-              <img
-                src="/img/javascript.png"
-                alt="javascript"
-                className="absolute w-24 opacity-20 blur-md filter md:w-56"
-              />
-              <img
-                src="/img/javascript.png"
-                alt="javascript"
-                className="w-20 md:w-36"
-                loading="lazy"
-              />
-            </div>
-          </li>
-          {/* ts */}
-          <li className="rounded-xl border bg-gradient-to-br from-white to-gray-50 p-5 text-xl duration-200 hover:shadow-lg dark:border-gray-700  dark:from-gray-700/75 dark:to-gray-800/75">
-            <div className="flex h-full w-full flex-col items-center justify-center gap-3">
-              <img
-                src="/img/typescript.png"
-                alt="php"
-                className="absolute w-24 opacity-20 blur-md filter md:w-56"
-              />
-              <img
-                src="/img/typescript.png"
-                alt="typescript"
-                className="w-20 md:w-36"
-                loading="lazy"
-              />
-            </div>
-          </li>
-          {/* php */}
-          <li className="rounded-xl border bg-gradient-to-br from-white to-gray-50 p-5 text-xl duration-200 hover:shadow-lg dark:border-gray-700  dark:from-gray-700/75 dark:to-gray-800/75">
-            <div className="flex h-full w-full flex-col items-center justify-center gap-3">
-              <img
-                src="/img/php.png"
-                alt="php"
-                className="absolute w-24 opacity-20 blur-md filter md:w-56"
-              />
-              <img
-                src="/img/php.png"
-                alt="php"
-                className="w-20 md:w-36"
-                loading="lazy"
-              />
-            </div>
-          </li>
-          {/* mysql */}
-          <li className="rounded-xl border bg-gradient-to-br from-white to-gray-50 p-5 text-xl duration-200 hover:shadow-lg dark:border-gray-700  dark:from-gray-700/75 dark:to-gray-800/75">
-            <div className="flex h-full w-full flex-col items-center justify-center gap-3">
-              <img
-                src="/img/mysql.png"
-                alt="mysql"
-                className="absolute w-24 opacity-20 blur-md filter md:w-56"
-              />
-              <img
-                src="/img/mysql.png"
-                alt="mysql"
-                className="w-20 md:w-36"
-                loading="lazy"
-              />
-            </div>
-          </li>
-        </ol>
-      </div>
+      <ol className="grid w-full list-none grid-cols-2 gap-4 text-lg text-gray-500 dark:text-gray-400 sm:grid-cols-4">
+        {/* js */}
+        <li className="rounded-xl border bg-gradient-to-br from-white to-gray-50 p-5 text-xl transition duration-200 hover:-translate-y-1 hover:shadow-lg  dark:border-gray-700  dark:from-gray-700/75 dark:to-gray-800/75">
+          <div className="flex h-full w-full flex-col items-center justify-center gap-3">
+            <img
+              src="/img/javascript.png"
+              alt="javascript"
+              className="absolute w-2 opacity-10 blur-md filter md:w-56"
+            />
+            <img
+              src="/img/javascript.png"
+              alt="javascript"
+              className="w-20 md:w-36"
+              loading="lazy"
+            />
+          </div>
+        </li>
+        {/* ts */}
+        <li className="rounded-xl border bg-gradient-to-br from-white to-gray-50 p-5 text-xl transition duration-200 hover:-translate-y-1 hover:shadow-lg  dark:border-gray-700  dark:from-gray-700/75 dark:to-gray-800/75">
+          <div className="flex h-full w-full flex-col items-center justify-center gap-3">
+            <img
+              src="/img/typescript.png"
+              alt="php"
+              className="absolute w-2 opacity-10 blur-md filter md:w-56"
+            />
+            <img
+              src="/img/typescript.png"
+              alt="typescript"
+              className="w-20 md:w-36"
+              loading="lazy"
+            />
+          </div>
+        </li>
+        {/* php */}
+        <li className="hover:shadow-l g rounded-xl border bg-gradient-to-br from-white to-gray-50 p-5 text-xl transition duration-200 hover:-translate-y-1 dark:border-gray-700  dark:from-gray-700/75 dark:to-gray-800/75">
+          <div className="flex h-full w-full flex-col items-center justify-center gap-3">
+            <img
+              src="/img/php.png"
+              alt="php"
+              className="absolute w-2 opacity-10 blur-md filter md:w-56"
+            />
+            <img
+              src="/img/php.png"
+              alt="php"
+              className="w-20 md:w-36"
+              loading="lazy"
+            />
+          </div>
+        </li>
+
+        {/* tailwind */}
+        <li className="rounded-xl border bg-gradient-to-br from-white to-gray-50 p-5 text-xl transition duration-200 hover:-translate-y-1 hover:shadow-lg  dark:border-gray-700  dark:from-gray-700/75 dark:to-gray-800/75">
+          <div className="flex h-full w-full flex-col items-center justify-center gap-3">
+            <img
+              src="/img/tailwind.png"
+              alt="tailwind"
+              className="absolute w-2 opacity-10 blur-md filter md:w-56"
+            />
+            <img
+              src="/img/tailwind.png"
+              alt="tailwind"
+              className="w-20 md:w-36"
+              loading="lazy"
+            />
+          </div>
+        </li>
+        {/* react */}
+        <li className="rounded-xl border bg-gradient-to-br from-white to-gray-50 p-5 text-xl transition duration-200 hover:-translate-y-1 hover:shadow-lg  dark:border-gray-700  dark:from-gray-700/75 dark:to-gray-800/75">
+          <div className="flex h-full w-full flex-col items-center justify-center gap-3">
+            <img
+              src="/img/reactjs.png"
+              alt="tailwind"
+              className="absolute w-2 opacity-10 blur-md filter md:w-56"
+            />
+            <img
+              src="/img/reactjs.png"
+              alt="react"
+              className="w-20 md:w-36"
+              loading="lazy"
+            />
+          </div>
+        </li>
+        {/* next */}
+        <li className="hover:shadow-l g transit200 rounded-xl border bg-gradient-to-br from-white to-gray-50 p-5 text-xl duration-200 hover:-translate-y-1 dark:border-gray-700  dark:from-gray-700/75 dark:to-gray-800/75">
+          <div className="flex h-full w-full flex-col items-center justify-center gap-3">
+            <img
+              src="/img/next.png"
+              alt="tailwind"
+              className="absolute w-2 opacity-10 blur-md filter md:w-56"
+            />
+            <img
+              src="/img/next.png"
+              alt="react"
+              className="w-20 md:w-36"
+              loading="lazy"
+            />
+          </div>
+        </li>
+      </ol>
     </section>
   );
 }
